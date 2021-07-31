2044e8781fb5ab7c3d840584e8932b58c53abdc9
yodasoda1219 <theo.github@bedafamily.com>
2021-07-30 18:30:51 -0700

    add framemanager

---
+++ GameOfLife/FrameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using GameOfLife.Frontends;
+
+namespace GameOfLife
+{
+    internal class FrameManager
+    {
+        public FrameManager()
+        {
+            FPS = 1;
+            CurrentFrameIndex = 0;
+            Paused = false;
+            mFrames = new();
+        }
+        public void AddFrame(HashSet<Vector> boardState)
+        {
+            mFrames.Add(boardState);
+        }
+        public void Update(IInputManager inputManager, HashSet<Vector>? boardState)
+        {
+            if (boardState != null)
+            {
+                AddFrame(boardState);
+            }
+            if (inputManager[Key.UpArrow].Down)
+            {
+                FPS++;
+            }
+            if (inputManager[Key.DownArrow].Down && FPS > 1)
+            {
+                FPS--;
+            }
+            if (inputManager[Key.Spacebar].Down)
+            {
+                Paused = !Paused;
+                if (!Paused)
+                {
+                    CurrentFrameIndex = 0;
+                }
+            }
+            if (inputManager[Key.LeftArrow].Down)
+            {
+                if (!Paused)
+                {
+                    Paused = true;
+                }
+                if (CurrentFrameIndex < mFrames.Count - 1)
+                {
+                    CurrentFrameIndex++;
+                }
+            }
+            if (inputManager[Key.RightArrow].Down)
+            {
+                if (!Paused)
+                {
+                    Paused = true;
+                }
+                if (CurrentFrameIndex > 0)
+                {
+                    CurrentFrameIndex--;
+                }
+            }
+        }
+        public HashSet<Vector> GetCurrentFrame()
+        {
+            return mFrames[^(CurrentFrameIndex + 1)];
+        }
+        public int FPS { get; set; }
+        public int CurrentFrameIndex { get; set; }
+        public bool Paused { get; set; }
+        private readonly List<HashSet<Vector>> mFrames;
+    }
+}
--- GameOfLife/Game.cs
+++ GameOfLife/Game.cs
@@ -1,4 +1,5 @@
-﻿using GameOfLife.Frontends;
+﻿using System.Collections.Generic;
+using GameOfLife.Frontends;
 
 namespace GameOfLife
 {
@@ -8,26 +9,39 @@ namespace GameOfLife
         {
             Frontend = frontend;
             Board = new Board(this);
+            FrameManager = new FrameManager();
             Running = false;
         }
         public void Run()
         {
+            // add the initial state
+            FrameManager.AddFrame(Board.Cells);
             Running = true;
             while (Running)
             {
-                var inputManager = Frontend.InputManager;
-                inputManager.Update();
-                if (inputManager[Key.Q].Down)
-                {
-                    Quit();
-                }
-                Board.Update();
+                Update();
                 Render();
             }
         }
+        private void Update()
+        {
+            var inputManager = Frontend.InputManager;
+            inputManager.Update();
+            if (inputManager[Key.Q].Down)
+            {
+                Quit();
+            }
+            HashSet<Vector>? boardState = null;
+            if (!FrameManager.Paused)
+            {
+                Board.Update();
+                boardState = Board.Cells;
+            }
+            FrameManager.Update(inputManager, boardState);
+        }
         private void Render()
         {
-            var cells = Board.Cells;
+            var cells = FrameManager.GetCurrentFrame();
             var renderer = Frontend.Renderer;
             renderer.BeginRender();
             foreach (Vector cell in cells)
@@ -43,5 +57,6 @@ namespace GameOfLife
         public bool Running { get; private set; }
         public IFrontend Frontend { get; private set; }
         public Board Board { get; private set; }
+        internal FrameManager FrameManager { get; private set; }
     }
 }
