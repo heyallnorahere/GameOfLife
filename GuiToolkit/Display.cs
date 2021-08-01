1bdc36309c53e15f4b521e3063f0b5b444e5a117
yodasoda1219 <theo.github@bedafamily.com>
2021-08-01 00:30:56 -0700

    start gui frontend

---
+++ GuiToolkit/Display.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace GuiToolkit
+{
+    public sealed class Display
+    {
+        public Display(string title, int width, int height)
+        {
+            var options = WindowOptions.DefaultVulkan; // we're gonna use this with vulkan
+            options.Size = new Vector2D<int>(width, height);
+            options.Title = title;
+            mWindow = Window.Create(options);
+            mWindow.Load += OnLoad;
+            mWindow.Update += OnUpdate;
+            mWindow.Render += OnRender;
+        }
+        private void OnLoad()
+        {
+            InputManager = new InputManager(mWindow.CreateInput());
+        }
+        private void OnUpdate(double obj)
+        {
+            InputManager?.UpdateAndSwapStates();
+            Update?.Invoke(obj);
+        }
+        private void OnRender(double obj)
+        {
+            Render?.Invoke(obj);
+        }
+        private readonly IWindow mWindow;
+        public InputManager? InputManager { get; private set; }
+        public event Action<double>? Update;
+        public event Action<double>? Render;
+    }
+}
---
+++ GuiToolkit/InputManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Silk.NET.Input;
+
+namespace GuiToolkit
+{
+    public struct KeyState
+    {
+        internal void Reset()
+        {
+            Up = false;
+            Down = false;
+            Held = false;
+        }
+        public bool Up { get; internal set; }
+        public bool Down { get; internal set; }
+        public bool Held { get; internal set; }
+    }
+    internal class InputState
+    {
+        public InputState()
+        {
+            KeyStates = new Dictionary<Key, KeyState>();
+            Reset();
+        }
+        public void Reset()
+        {
+            foreach (var key in Enum.GetValues<Key>())
+            {
+                var keyState = new KeyState();
+                keyState.Reset();
+                KeyStates[key] = keyState;
+            }
+        }
+        public Dictionary<Key, KeyState> KeyStates { get; private set; }
+    }
+    public sealed class InputManager
+    {
+        public InputManager(IInputContext inputContext, int states = 2)
+        {
+            mInputStates = new Queue<InputState>();
+            for (int i = 0; i < states; i++)
+            {
+                mInputStates.Enqueue(new InputState());
+            }
+            mInputContext = inputContext;
+        }
+        internal void UpdateAndSwapStates()
+        {
+            InputState HEAD = mInputStates.Dequeue();
+            InputState? lastHEAD = mHEAD;
+            mHEAD = HEAD;
+            foreach (var key in Enum.GetValues<Key>())
+            {
+                var keyState = mHEAD.KeyStates[key];
+                for (int i = 0; i < mInputContext.Keyboards.Count; i++)
+                {
+                    var keyboard = mInputContext.Keyboards[i];
+                    if (keyboard.IsKeyPressed(key) && !keyState.Held)
+                    {
+                        keyState.Held = true;
+                    }
+                }
+                bool lastHeld = lastHEAD?.KeyStates[key].Held ?? false;
+                keyState.Down = keyState.Held && !lastHeld;
+                keyState.Up = !keyState.Held && lastHeld;
+                mHEAD.KeyStates[key] = keyState;
+            }
+            mInputStates.Enqueue(mHEAD);
+        }
+        public KeyState GetKey(Key key)
+        {
+            return mHEAD?.KeyStates[key] ?? throw new NullReferenceException();
+        }
+        public KeyState this[Key key] => GetKey(key);
+        private readonly IInputContext mInputContext;
+        private readonly Queue<InputState> mInputStates;
+        private InputState? mHEAD;
+    }
+}
