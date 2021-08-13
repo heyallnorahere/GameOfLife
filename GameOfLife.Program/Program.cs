d059251397af1cdee79bebc4f50c79e35f661b81
yodasoda1219 <theo.github@bedafamily.com>
2021-08-12 22:58:13 -0700

    add config file option

--- GameOfLife.Program/Program.cs
+++ GameOfLife.Program/Program.cs
@@ -11,6 +11,8 @@ namespace GameOfLife.Program
         {
             [Option('f', "frontend", HelpText = "Change the frontend running this application", Default = "Console")]
             public string? Frontend { get; set; }
+            [Option('c', "config", HelpText = "Change the config file that this application reads from", Default = "config.json")]
+            public string? ConfigFile { get; set; }
         }
         public static void Main(string[] args)
         {
@@ -18,22 +20,20 @@ namespace GameOfLife.Program
             var frontend = Frontend.Create(mFrontend ?? throw new NullReferenceException());
             frontend.SetupGameInstance += (Game instance) =>
             {
-                Config.Load();
+                Config.Load(mConfigFile ?? throw new NullReferenceException());
                 instance.Board.LoadConfig();
             };
             frontend.Run();
         }
         private static void OnParsed(Options options)
         {
-            if (options.Frontend != null)
-            {
-                mFrontend = options.Frontend;
-            }
+            mFrontend = options.Frontend;
+            mConfigFile = options.ConfigFile;
         }
         private static void OnError(IEnumerable<Error> errors)
         {
             throw new NullReferenceException();
         }
-        private static string? mFrontend;
+        private static string? mFrontend, mConfigFile;
     }
 }
